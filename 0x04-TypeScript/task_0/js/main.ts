76bc4e0e73ae914b3b92e54fabb7a6b3d6ec73ca
Githu77 <githubenard16@gmail.com>
2024-06-05 12:15:19 +0300

    add files

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const [student1, student2] = [
+    {
+        firstName: "Gon",
+        lastName: "Freecs",
+        age: 12,
+        location: "New-York"
+    },
+    {
+        firstName: "Suzan",
+        lastName: "Storm",
+        age: 30,
+        location: "Amsterdam"
+    }
+]
+
